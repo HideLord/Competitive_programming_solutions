66420365144220bc1f0b35e65ad07639f8023b70
= <=>
2019-11-16 13:29:28 +0200

    Week 2

---
+++ Week 2/Suffix_automat_solution.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define all(x) (x).begin(),(x).end()
+#define sz(x) int((x).size())
+#define mp make_pair
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> ii;
+typedef vector<ii> vii;
+
+template<class T>
+ostream& operator<<(ostream& out, vector<T> V){
+    for(auto &i:V)out << i << " ";
+    return out;
+}
+
+
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+    
+}
