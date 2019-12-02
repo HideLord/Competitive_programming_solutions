7f4adbcf9c1fa6e5437d276e9840c33e38897fee
HideLord <polimonom@gmail.com>
2019-12-02 09:57:58 +0200

    Sqrt_decomposition_solution

---
+++ Week 4/Sqrt_decomposition_solution.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+#define all(x) (x).begin(),(x).end()
+#define sz(x) int((x).size())
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> ii;
+
+template<class T, class U>
+ostream& operator<<(ostream& out, pair<T,U> &P){
+    return out << "[" << P.first << ", " << P.second << "]";
+}
+
+template<class T>
+ostream& operator<<(ostream& out, vector<T> &V){
+    cout << '[';
+    for(int i(0); i < sz(V); i++)out << V[i] << (i+1==sz(V)?"]":", ");
+    return out;
+}
+
+template<class T> struct Node{
+    vector<T> vals;
+    #define def 0
+    T res = def;
+    #define op(a,b) ((a)+(b)) // The operation should be commutative
+    Node(const int SZ){
+        vals.assign(SZ,def);
+    }
+    constexpr int size() const {return sz(vals);}
+    void reeval(){
+        res = def;
+        for(auto &i:vals) res = op(res,i);
+    }
+    void modify(int ind, T val){
+        vals[ind] = val;
+        reeval();
+    }
+    T query(int l, int r){// [l,r)
+        T res = def;
+        while(l<r)res = op(res,vals[l++]);
+        return res;
+    }
+    T query(){ return res; }
+};
+template<class T> struct sqrtD{
+    list<Node<T>> nodes;
+    sqrtD(vector<int> &V){
+        int nodeSize = ceil(sqrt(sz(V)));
+        Node<T> n(nodeSize);
+        for(int i = 0, curr = 0; i < sz(V); i++,curr++){
+            if(curr==nodeSize){
+                curr = 0;
+                n.reeval();
+                nodes.push_back(n);
+                n.vals.assign(nodeSize,def);
+            }
+            n.vals[curr]=V[i];
+        }
+        n.reeval();
+        nodes.push_back(n);
+    }
+    void modify(int ind, T val){
+        int curr = 0;
+        for(auto &n: nodes){
+            if(curr<=ind && curr+sz(n)>ind){
+                n.modify(ind-curr, val);
+                break;
+            }
+            curr+=sz(n);
+        }
+    }
+    T query(int l, int r){// [l,r)
+        int curr = 0;
+        T res = def;
+        for(auto &n: nodes){
+            if(curr<=l && curr+sz(n)>l){
+                if(curr<=r && curr+sz(n)>=r)
+                    return n.query(l-curr,r-curr);
+                res = op(n.query(l-curr,sz(n)),res);
+            }else if(curr>l && curr+sz(n)<=r){
+                res = op(n.query(),res);
+            }else if(curr<r && curr+sz(n)>=r){
+                res = op(n.query(0,r-curr),res);
+                return res;
+            }
+            curr+=sz(n);
+        }
+        return res;
+    }
+};
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+    vector<int> V(100);
+    iota(all(V),1);
+    sqrtD<int> sd(V);
+    int real_res = 0, l=2,r=23;
+    sd.modify(13,0);
+    V[13] = 0;
+    for(int i = l; i < r; i++)real_res=op(V[i],real_res);
+    cout << "wanted: " << real_res << endl;
+    cout << sd.query(l,r) << endl;
+}
